886f7b3f63a94678eff68ca2b5c5c3b825eab786
дилшод <ddilshod2197@gmail.com>
2026-05-09 13:21:00 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,7 @@
+def decorator(func):
+    def wrapper(*args, **kwargs):
+        print("Before")
+        result = func(*args, **kwargs)
+        print("After")
+        return result
+    return wrapper
